5044312860b3ad31fcea53c66ff43d86fdb77878
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2022-12-11 16:35:07 +0100

    Task_tracker

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Taask Tracker</h1>
+            <title>Task tracker</title>
+        </div>
+    );
+};
+
+export default Header;
